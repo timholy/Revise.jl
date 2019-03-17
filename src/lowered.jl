bcd6d723ff349a1a0bf08b1555a6384e74e6927f
Lyndon White <oxinabox@ucc.asn.au>
2019-03-17 17:13:41 -0500

    Use exception kw in atsign-warn
    Co-Authored-By: timholy <tim.holy@gmail.com>

--- src/lowered.jl
+++ src/lowered.jl
@@ -40,7 +40,7 @@ function methods_by_execution!(@nospecialize(recurse), methodinfo, docexprs, mod
         return methods_by_execution!(recurse, methodinfo, docexprs, frame; define=define)
     catch err
         isa(err, InterruptException) && rethrow(err)
-        @warn "error evaluating in module $mod: $ex"
+        @warn "error evaluating in module $mod" exception=ex
         return nothing
     end
 end
