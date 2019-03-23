ae8e0f4375c9c570c0be4f5c207753732df6faaa
Tim Holy <tim.holy@gmail.com>
2019-03-23 10:59:09 -0500

    Don't error on empty source files

--- src/Revise.jl
+++ src/Revise.jl
@@ -583,7 +583,7 @@ function track(mod::Module, file::AbstractString)
     isfile(file) || error(file, " is not a file")
     file = normpath(abspath(file))
     fm = parse_source(file, mod)
-    if fm != nothing
+    if fm !== nothing
         instantiate_sigs!(fm)
         id = PkgId(mod)
         if !haskey(pkgdatas, id)
