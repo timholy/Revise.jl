c6bcb855505bb799d8110ef0ee6ff1c0b8a62cd3
Tim Holy <tim.holy@gmail.com>
2019-05-12 09:00:28 -0500

    Add a test for inotify warning

---
+++ test/inotify.jl
@@ -0,0 +1,11 @@
+using Revise, Test
+# This test should only be run if you have a very small inotify limit
+
+@testset "inotify" begin
+    logs, _ = Test.collect_test_logs() do
+        Revise.track("common.jl")
+    end
+    sleep(0.1)
+    @test !isempty(logs)
+    @test any(rec->occursin("inotify", rec.message), logs)
+end
