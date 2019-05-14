69465c32d1206ebfbd906d0a83d692ad4a1a3b8e
Magnus Lie Hetland <magnus@hetland.org>
2019-05-14 16:25:53 +0200

    Language tweak
    Co-Authored-By: Tim Holy <tim.holy@gmail.com>

--- src/Revise.jl
+++ src/Revise.jl
@@ -662,7 +662,7 @@ Execute `f()` whenever files listed in `files`, or code in `modules`, updates.
 `entr` will process updates (and block your command line) until you press Ctrl-C.
 Unless `postpone` is `true`, `f()` will be executed also when calling `entr`,
 regardless of file changes. The `pause` is the period (in seconds) that `entr`
-will wait between it has been triggered and actually calling `f()`, to handle
+will wait between being triggered and actually calling `f()`, to handle
 clusters of modifications, such as those produced by saving files in certain
 text editors.
 
