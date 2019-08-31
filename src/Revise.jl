b5331b1a048a6e77428c63fcdd56c331b8bac14a
Tim Holy <tim.holy@gmail.com>
2019-08-31 14:57:56 -0500

    Don't call `revise()` for `exit()` (fixes #327)

--- src/Revise.jl
+++ src/Revise.jl
@@ -945,8 +945,10 @@ end
             # exit flag
             break
         end
-        # Process revisions
-        revise(backend)
+        # Process revisions, skipping `exit()` (issue #327)
+        if length(ast.args) < 2 || (ex = ast.args[2]; !isexpr(ex, :call)) || length(ex.args) != 1 || ex.args[1] != :exit
+            revise(backend)
+        end
         # Now eval the input
         REPL.eval_user_input(ast, backend)
     end
