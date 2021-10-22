db11d0d3cdb3052b44356350680c8498e57c9af1
Xubo Leng <hsupo.leng@gmail.com>
2021-10-22 12:30:22 -0500

    Update in response to review comments

--- src/Revise.jl
+++ src/Revise.jl
@@ -1,7 +1,7 @@
 """
 Revise.jl tracks source code changes and incorporates the changes to a running Julia session. 
 
-Revise.jl works behind-the-scene. To track a package, e.g. `Example`:
+Revise.jl works behind-the-scenes. To track a package, e.g. `Example`:
 ```julia
 (@v1.6) pkg> dev Example        # make a development copy of the package
 [...pkg output omitted...]
@@ -10,17 +10,15 @@ julia> using Revise             # this must come before the package under develo
 
 julia> using Example            
 
-[...develop the package...]
-
-(@v1.6) pkg> free Example       # Revise.jl will automatically track the package's released version
+[...develop the package...]     # Revise.jl will automatically update package functionality to match code changes  
 
 ```
 
-Functions in Revise.jl that may come handy:
-- `revise`: evaluate any changes in `Revise.revision_queue` or every definition in a module
+Functions in Revise.jl that may come handy in special circumstances:
 - `Revise.track`: track updates to `Base` Julia itself or `Core.Compiler`
 - `includet`: load a file and track future changes. Intended for small, quick works
 - `entr`: call an additional function whenever code updates
+- `revise`: evaluate any changes in `Revise.revision_queue` or every definition in a module
 - `Revise.retry`: perform previously-failed revisions. Useful in cases of order-dependent errors
 - `Revise.errors`: report the errors represented in `Revise.queue_errors`
 """
