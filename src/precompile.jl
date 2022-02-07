cf42ad18e4ac8262387216c5c3153ebd5a7d8249
Tim Holy <tim.holy@gmail.com>
2022-02-07 15:24:08 -0600

    Rename private module

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,4 +1,4 @@
-module __RInternal__
+module var"#Internal"
 ftmp() = 1
 end
 
@@ -23,12 +23,12 @@ function _precompile_()
     add_revise_deps(true)
     revise()
     revise_first(:(1+1))
-    eval_with_signatures(__RInternal__, :(f() = 1))
-    eval_with_signatures(__RInternal__, :(f2() = 1); skip_include=true)
+    eval_with_signatures(var"#Internal", :(f() = 1))
+    eval_with_signatures(var"#Internal", :(f2() = 1); skip_include=true)
     add_require(pathof(LoweredCodeUtils), LoweredCodeUtils, "295af30f-e4ad-537b-8983-00126c2a3abe", "Revise", :(include("somefile.jl")))
     add_require(pathof(JuliaInterpreter), JuliaInterpreter, "295af30f-e4ad-537b-8983-00126c2a3abe", "Revise", :(f(x) = 7))
     pkgdata = pkgdatas[PkgId(LoweredCodeUtils)]
-    eval_require_now(pkgdata, length(pkgdata.info.files), last(pkgdata.info.files)*"__@require__", joinpath(basedir(pkgdata), last(pkgdata.info.files)), Revise, :(__RInternal__.ftmp(::Int) = 0))
+    eval_require_now(pkgdata, length(pkgdata.info.files), last(pkgdata.info.files)*"__@require__", joinpath(basedir(pkgdata), last(pkgdata.info.files)), Revise, :(var"#Internal".ftmp(::Int) = 0))
     # Now empty the stores to prevent them from being serialized
     empty!(watched_files)
     empty!(watched_manifests)
