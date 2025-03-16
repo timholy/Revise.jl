c90041b3807d911d1afb0304ea2b0659b85b9211
Tim Holy <tim.holy@gmail.com>
2025-03-16 07:17:33 -0500

    delete debugging code

--- src/packagedef.jl
+++ src/packagedef.jl
@@ -833,8 +833,6 @@ function revise(; throw::Bool=false)
                         maybe_extract_sigs_for_meths(newmeths)
                         union!(reeval_methods, newmeths)
                     end
-                else
-                    push!(trouble, m)
                 end
             end
         end
@@ -871,8 +869,6 @@ function revise(; throw::Bool=false)
     nothing
 end
 revise(backend::REPL.REPLBackend) = revise()
-
-trouble = []
 
 """
     revise(mod::Module; force::Bool=true)
