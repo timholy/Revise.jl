5068157ba0912a42cc5b132b190205574b80b121
Tim Holy <tim.holy@gmail.com>
2025-04-12 03:55:57 -0500

    Avoid redefining types unless required

--- test/runtests.jl
+++ test/runtests.jl
@@ -3130,7 +3130,6 @@ const issue639report = []
 
         @test !isempty(methods(Core.Compiler.NativeInterpreter))
 
-        #=
         if !haskey(ENV, "BUILDKITE") # disable on buildkite, see discussion in https://github.com/JuliaCI/julia-buildkite/pull/372#issuecomment-2262840304
             # Determine whether a git repo is available. Travis & Appveyor do not have this.
             repo, path = Revise.git_repo(Revise.juliadir)
@@ -3147,7 +3146,6 @@ const issue639report = []
                 @warn "skipping Core.Compiler tests due to lack of git repo"
             end
         end
-        =#
 
         @test !isempty(methods(Core.Compiler.NativeInterpreter))
     end
