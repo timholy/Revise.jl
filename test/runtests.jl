f5cf5b87f560659824b318431c1ba61bca9e72d0
Tim Holy <tim.holy@gmail.com>
2025-08-09 04:46:16 -0500

    Force some precompilation

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,6 +23,10 @@ Pkg.precompile()
 # *.ji files for the package.
 using EponymTuples
 
+# Also ensure packages that we'll `@require` are precompiled, as otherwise Pkg `@info`
+# may contaminate the log and cause test failures.
+Pkg.precompile(["EndpointRanges", "CatIndices", "IndirectArrays", "RoundingIntegers", "UnsafeArrays"])
+
 include("common.jl")
 
 throwing_function(bt) = bt[2]
