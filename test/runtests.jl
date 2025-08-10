375d9c97c4438e83b005d42c2a7304579efceae9
Tim Holy <tim.holy@gmail.com>
2025-08-10 05:21:18 -0500

    Back out the test-package precompilaton
    Was causing failures in extra tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,9 +18,9 @@ using Base.CoreLogging: Debug,Info
 # *.ji files for the package.
 using EponymTuples
 
-# Also ensure packages that we'll `@require` are precompiled, as otherwise Pkg `@info`
-# may contaminate the log and cause test failures.
-Pkg.precompile(["EndpointRanges", "CatIndices", "IndirectArrays", "RoundingIntegers", "UnsafeArrays"])
+# # Also ensure packages that we'll `@require` are precompiled, as otherwise Pkg `@info`
+# # may contaminate the log and cause test failures.
+# Pkg.precompile(["EndpointRanges", "CatIndices", "IndirectArrays", "RoundingIntegers", "UnsafeArrays"])
 
 include("common.jl")
 
